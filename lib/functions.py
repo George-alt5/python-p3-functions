ffe2f0a7a5e1d70e8966eadef5e913e46e5b419f
George-alt5 <georgekihoro6@gmail.com>
2025-08-05 12:56:48 +0300

    Finished Lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-
+name = "Sunny"
+add = (1,2)
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name = "Naureen"):
+    print(f"Hello, {name}!")
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
